3d891215e3569c9a45e4b0f7dc1364d48f9db8c1
M.S. Babaei <babaei@babaei-pc.localhost>
2014-02-21 17:42:03 +0330

    Updated CoreLib

--- CoreLib/crypto.cpp
+++ CoreLib/crypto.cpp
@@ -1,10 +1,9 @@
+#include <algorithm>
 #include <stdexcept>
-
 #if defined ( _WIN32 )
 #include <windows.h>
 //#include <cryptopp/dll.h>     // msvc-shared only
 #endif  // defined ( _WIN32 )
-
 #include <cryptopp/aes.h>
 #include <cryptopp/ccm.h>
 #include <cryptopp/cryptlib.h>
@@ -14,7 +13,7 @@
 #include <b64/decode.h>
 #include <b64/encode.h>
 #include "crypto.hpp"
-#include "log.hpp"
+#include "make_unique.hpp"
 
 
 #define     UNKNOWN_ERROR           "CoreLib::Crypto unknown error!"
@@ -27,39 +26,31 @@ using namespace CoreLib;
 
 struct Crypto::Impl
 {
-    // Use this nice HEX/ASCII converter and your editor's replace dialog,
-    // to create your own Key and IV.
-    // http://www.dolcevie.com/js/converter.html
+    Crypto::Byte_t *Key;
+    std::size_t KeyLen;
+    Crypto::Byte_t *IV;
+    std::size_t IVLen;
 
-    static constexpr unsigned char KEY[] = {
-        0x4e, 0x40, 0x21, 0x6b, 0x21, 0x30, 0x0, 0x2d, 0x40, 0x6b, 0x23, 0x48, 0x2f, 0x2e, 0x3f, 0x71
-    };
-
-    static constexpr unsigned char IV[] = {
-        0x53, 0x23, 0x33, 0x54, 0x34, 0x5f, 0x2b, 0x52, 0x33, 0x40, 0x48, 0x3f, 0x2e, 0x71, 0x0, 0x2f
-    };
+    Impl();
+    ~Impl();
 };
 
-bool Crypto::Encrypt(const char *plainText, std::string &out_encodedText)
+bool Crypto::Encrypt(const std::string &plainText, std::string &out_encodedText,
+                     const Byte_t *key, std::size_t keyLen, const Byte_t *iv, std::size_t ivLen)
 {
     string err;
-    return Encrypt(plainText, out_encodedText, err);
+    return Crypto::Encrypt(plainText.c_str(), out_encodedText, err, key, keyLen, iv, ivLen);
 }
 
-bool Crypto::Encrypt(const std::string &plainText, std::string &out_encodedText)
-{
-    string err;
-    return Encrypt(plainText.c_str(), out_encodedText, err);
-}
-
-bool Crypto::Encrypt(const char *plainText, std::string &out_encodedText,
-                     std::string &out_error)
+bool Crypto::Encrypt(const std::string &plainText, std::string &out_encodedText,
+                     std::string &out_error,
+                     const Byte_t *key, std::size_t keyLen, const Byte_t *iv, std::size_t ivLen)
 {
     try {
         string cipher;
 
         CBC_Mode<AES>::Encryption enc;
-        enc.SetKeyWithIV(Impl::KEY, sizeof(Impl::KEY), Impl::IV, sizeof(Impl::IV));
+        enc.SetKeyWithIV(key, keyLen, iv, ivLen);
 
         cipher.clear();
         StringSource(plainText, true,
@@ -72,49 +63,36 @@ bool Crypto::Encrypt(const char *plainText, std::string &out_encodedText,
     }
 
     catch (const CryptoPP::Exception &ex) {
-        LOG_ERROR(ex.what());
         out_error.assign(ex.what());
     }
 
     catch (const std::exception &ex) {
-        LOG_ERROR(ex.what());
         out_error.assign(ex.what());
     }
 
     catch (...) {
-        LOG_ERROR(UNKNOWN_ERROR);
         out_error.assign(UNKNOWN_ERROR);
     }
 
     return false;
 }
 
-bool Crypto::Encrypt(const std::string &plainText, std::string &out_encodedText,
-                     std::string &out_error)
-{
-    return Encrypt(plainText.c_str(), out_encodedText, out_error);
-}
-
-bool Crypto::Decrypt(const char *cipherText, std::string &out_recoveredText)
+bool Crypto::Decrypt(const std::string &cipherText, std::string &out_recoveredText,
+                     const Byte_t *key, std::size_t keyLen, const Byte_t *iv, std::size_t ivLen)
 {
     string err;
-    return Decrypt(cipherText, out_recoveredText, err);
+    return Crypto::Decrypt(cipherText.c_str(), out_recoveredText, err, key, keyLen, iv, ivLen);
 }
 
-bool Crypto::Decrypt(const std::string &cipherText, std::string &out_recoveredText)
-{
-    string err;
-    return Decrypt(cipherText.c_str(), out_recoveredText, err);
-}
-
-bool Crypto::Decrypt(const char *cipherText, std::string &out_recoveredText,
-                     std::string &out_error)
+bool Crypto::Decrypt(const std::string &cipherText, std::string &out_recoveredText,
+                     std::string &out_error,
+                     const Byte_t *key, std::size_t keyLen, const Byte_t *iv, std::size_t ivLen)
 {
     try {
         string cipher;
 
         CBC_Mode<AES>::Decryption dec;
-        dec.SetKeyWithIV(Impl::KEY, sizeof(Impl::KEY), Impl::IV, sizeof(Impl::IV));
+        dec.SetKeyWithIV(key, keyLen, iv, ivLen);
 
         cipher.clear();
         StringSource(cipherText, true, new HexDecoder(new StringSink(cipher)));
@@ -127,27 +105,18 @@ bool Crypto::Decrypt(const char *cipherText, std::string &out_recoveredText,
     }
 
     catch (const CryptoPP::Exception &ex) {
-        LOG_ERROR(ex.what());
         out_error.assign(ex.what());
     }
 
     catch (const std::exception &ex) {
-        LOG_ERROR(ex.what());
         out_error.assign(ex.what());
     }
 
     catch (...) {
-        LOG_ERROR(UNKNOWN_ERROR);
         out_error.assign(UNKNOWN_ERROR);
     }
 
     return false;
-}
-
-bool Crypto::Decrypt(const std::string &cipherText, std::string &out_recoveredText,
-                     std::string &out_error)
-{
-    return Decrypt(cipherText.c_str(), out_recoveredText, out_error);
 }
 
 
@@ -177,17 +146,14 @@ bool Crypto::GenerateHash(const char *text, std::string &out_digest,
     }
 
     catch (const CryptoPP::Exception &ex) {
-        LOG_ERROR(ex.what());
         out_error.assign(ex.what());
     }
 
     catch (const std::exception &ex) {
-        LOG_ERROR(ex.what());
         out_error.assign(ex.what());
     }
 
     catch (...) {
-        LOG_ERROR(UNKNOWN_ERROR);
         out_error.assign(UNKNOWN_ERROR);
     }
 
@@ -240,5 +206,56 @@ void Crypto::Base64Encode(std::istream &inputStream, std::ostream &outputStream)
 {
     base64::encoder encoder;
     encoder.encode(inputStream, outputStream);
+}
+
+Crypto::Crypto(const Byte_t *key, std::size_t keyLen, const Byte_t *iv, std::size_t ivLen) :
+    m_pimpl(std::make_unique<Crypto::Impl>())
+{
+    m_pimpl->Key = new Byte_t[keyLen];
+    m_pimpl->IV = new Byte_t[ivLen];
+
+    std::copy(key, key + keyLen, m_pimpl->Key);
+    std::copy(iv, iv + ivLen, m_pimpl->IV);
+
+    m_pimpl->KeyLen = keyLen;
+    m_pimpl->IVLen = ivLen;
+}
+
+Crypto::~Crypto()
+{
+
+}
+
+bool Crypto::Encrypt(const std::string &plainText, std::string &out_encodedText)
+{
+    return Crypto::Encrypt(plainText, out_encodedText, m_pimpl->Key, m_pimpl->KeyLen, m_pimpl->IV, m_pimpl->IVLen);
+}
+
+bool Crypto::Encrypt(const std::string &plainText, std::string &out_encodedText,
+                     std::string &out_error)
+{
+    return Crypto::Encrypt(plainText, out_encodedText, out_error, m_pimpl->Key, m_pimpl->KeyLen, m_pimpl->IV, m_pimpl->IVLen);
+}
+
+bool Crypto::Decrypt(const std::string &cipherText, std::string &out_recoveredText)
+{
+    return Crypto::Decrypt(cipherText, out_recoveredText, m_pimpl->Key, m_pimpl->KeyLen, m_pimpl->IV, m_pimpl->IVLen);
+}
+
+bool Crypto::Decrypt(const std::string &cipherText, std::string &out_recoveredText,
+                     std::string &out_error)
+{
+    return Crypto::Encrypt(cipherText, out_recoveredText, out_error, m_pimpl->Key, m_pimpl->KeyLen, m_pimpl->IV, m_pimpl->IVLen);
+}
+
+Crypto::Impl::Impl()
+{
+
+}
+
+Crypto::Impl::~Impl()
+{
+    delete IV;
+    delete Key;
 }
 
--- CoreLib/mail.hpp
+++ CoreLib/mail.hpp
@@ -1,7 +1,8 @@
-#ifndef MAILER_HPP
-#define MAILER_HPP
+#ifndef CORELIB_MAILER_HPP
+#define CORELIB_MAILER_HPP
 
 
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -40,6 +41,6 @@ public:
 };
 
 
-#endif /* MAILER_HPP */
+#endif /* CORELIB_MAILER_HPP */
 
 
